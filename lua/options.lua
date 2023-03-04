647a3198f0ad2e3ceadfdfbe8e87dccfbbf17277
okobenoko <okobenoko@outlook.com>
2023-03-04 17:36:08 +0800

    “commit11.2”

--- lua/options.lua
+++ lua/options.lua
@@ -19,3 +19,23 @@ opt.splitbelow = true
 opt.termguicolors = true
 opt.signcolumn = "yes"
 
+-- Close new line comment
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+    end,
+})
+
+-- Create missing directory by command "MakeDirectory"
+vim.api.nvim_create_user_command("MakeDirectory", function()
+    ---@diagnostic disable-next-line: missing-parameter
+    local path = vim.fn.expand("%")
+    local dir = vim.fn.fnamemodify(path, ":p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+        vim.fn.mkdir(dir, "p")
+    else
+        vim.notify("Directory already exists", "WARN", { title = "Nvim" })
+    end
+end, { desc = "Create directory if it doesn't exist" })
+
